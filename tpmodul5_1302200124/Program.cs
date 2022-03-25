1f9131c5a78346e320b6ff01f02a1be565b7394f
Hilmi FC <hilmie123013@gmail.com>
2022-03-25 15:37:49 +0700

    Menambahkan Class SayaTubeVideo

--- tpmodul5_1302200124/Program.cs
+++ tpmodul5_1302200124/Program.cs
@@ -10,6 +10,39 @@ namespace tpmodul5_1302200124
     {
         static void Main(string[] args)
         {
+            SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract – [HILMI FADHILLAH CAHYADI]");
+            video.PrintVideoDetails();
+            video.IncreasePlayCount(1);
+            video.PrintVideoDetails();
+        }
+    }
+    public class SayaTubeVideo
+    {
+        private int id;
+        private string title;
+        private int playCount;
+
+        public SayaTubeVideo(string judul)
+        {
+           
+            Random ids = new Random();
+            this.title = judul;
+            id = ids.Next(0, 100000);
+            this.playCount = 0;
+        }
+        public void IncreasePlayCount(int i)
+        {
+            
+            playCount = playCount + i;
+
+
+        }
+
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine(this.id);
+            Console.WriteLine(this.title);
+            Console.WriteLine(this.playCount);
         }
     }
 }
